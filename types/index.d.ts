54fb310bd56f65259e8f2e4c82117f3c27ca4ce5
VadimMalykhin <158365+vadimmalykhin@users.noreply.github.com>
2022-01-21 07:17:24 +0300

    ✨

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,16 @@
-declare module 'binance-icons' {  /**   * Binance Crypto Icons   */  export const binanceCryptoIcons: Map<string, string>  /**   * Binance Currency Icons   */  export const binanceCurrencyIcons: Map<string, string>  /**   * Binance ETF Icons   */  export const binanceEtfIcons: Map<string, string>}
+declare module 'binance-icons' {
+  /**
+   * Binance Crypto Icons
+   */
+  export const binanceCryptoIcons: Map<string, string>
+
+  /**
+   * Binance Currency Icons
+   */
+  export const binanceCurrencyIcons: Map<string, string>
+
+  /**
+   * Binance ETF Icons
+   */
+  export const binanceEtfIcons: Map<string, string>
+}
